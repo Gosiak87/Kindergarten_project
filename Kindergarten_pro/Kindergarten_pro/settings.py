7bc5d5ef6b3fb320d176035d528654bafe5659f6
Gosiak87 <miziolekgosia@gmail.com>
2018-03-18 13:17:53 +0100

    add data_base

--- Kindergarten_pro/Kindergarten_pro/settings.py
+++ Kindergarten_pro/Kindergarten_pro/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Kindergarten_app',
 ]
 
 MIDDLEWARE = [
@@ -75,12 +76,13 @@ WSGI_APPLICATION = 'Kindergarten_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'kindergarten_base',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': '127.0.01'
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
